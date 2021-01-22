9495be1e2698e68b259f4381369d389f6494d9c1
searsmg <megan.sears@colostate.edu>
2021-01-22 15:46:43 -0700

    1/22/22 get sunrise, solar noon, and sunset times per day based on location using suncalc package

--- Suncalc_forlapserates.R
+++ Suncalc_forlapserates.R
@@ -9,5 +9,36 @@ setwd("C:/Users/sears/Documents/Research/Snow_Hydro_Research/BM Sensors/Field_Da
 BM <- read.csv("C:/Users/sears/Documents/Research/Snow_Hydro_Research/BM Sensors/Field_Data_011821/Processed/MP_HD_All_Jan21.csv")%>%
   mutate(datetime = mdy_hm(datetime))
 
-date <-
+date <- seq.Date(from = as.Date("2020-04-19", tz="MST"),
+                 to=as.Date("2021-01-14", tz="MST"),
+                 by = 1)
+
+sunlightdata <- getSunlightTimes(date=date,
+                                 lat=40.544,
+                                 lon=-105.887,
+                                 tz="MST",
+                                 keep=c("sunrise", "solarNoon", "sunset"))
+
+BM_daily <- BM %>%
+  mutate(day = as.Date(datetime, format="%Y-%m-%d")) %>%
+  group_by(ID, Elevation,day) %>%
+  summarise(daily_avgtemp = mean(temperature))
+
+write.csv(BM_daily, "DailyAvgT.csv")
+
+
+BM <- BM %>%
+  mutate(date = as.Date(datetime, format="%Y-%m-%d"))
+  
+BM_light <- merge(BM, sunlightdata, by="date")
+
+BM_light <- BM_light %>%
+  mutate(time = (datetime, format="%H:%M:%S"))
+
+BM_light <- BM_light %>%
+  mutate(light=if_else(datetime>sunrise & datetime<sunset,"day", "night"))
+
+write.csv(BM_light, "BM_light.csv")
+
+
 
