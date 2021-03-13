c97e77d91ed94f8ee6e98fa13de377a168e49e9a
searsmg <megan.sears@colostate.edu>
2021-03-13 15:39:00 -0700

    few minor updates, added DEM and plot

--- JW_spatial.R
+++ JW_spatial.R
@@ -7,18 +7,21 @@ library(gdalUtils)
 library(raster)
 library(sp)
 
+
 rm(list = ls())
 
   
 # add DEM:
 dem  <- raster("DEM/ned30m40105.tif")
 
-# Setup an output filename (shapefile):
-output_shapefile <- "input_dem.shp" #paste(tempfile(),".shp",sep="")
+# the distribution of values in the raster
+hist(dem, main="Distribution of elevation values", 
+     col= "purple", 
+     maxpixels=22000000)
 
-contour_output <- gdal_contour(src_filename=input_dem,dst_filename=input_dem,
-                               a="Elevation",i=5.,output_Vector=TRUE)
-# Plot the contours using spplot:
-spplot(contour_output["Elevation"],contour=TRUE)
-}
-# }
+plot(dem, 
+     main="DEM JW (need to clip)") # add title with main
+cropbox1 <- drawExtent()
+
+image(dem)
+
