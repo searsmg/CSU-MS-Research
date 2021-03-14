25b7da3747925ddf917737582920251de6f37d07
searsmg <megan.sears@colostate.edu>
2021-03-13 17:48:20 -0700

    clipped DEM to JW boundary, added sensor points in

--- JW_spatial.R
+++ JW_spatial.R
@@ -6,6 +6,10 @@ library(rgdal)
 library(gdalUtils)
 library(raster)
 library(sp)
+library(streamstats)
+library(sf)
+library(rgeos)
+library(ggplot2)
 
 
 rm(list = ls())
@@ -25,3 +29,37 @@ cropbox1 <- drawExtent()
 
 image(dem)
 
+test <- delineateWatershed(xlocation = -105.882005, ylocation = 40.540477, crs = 4326,
+                          includeparameters = "true")
+
+leafletWatershed(JWboundary)
+
+writeShapefile(watershed = test, layer = "layer_name", dir = "ws1_shp", what = "boundary")
+
+JWboundary <- readOGR(dsn = "ws1_shp", layer = "layer_name")
+JW_sensors <- readOGR(dsn="sensors", layer="BMsensors_all")
+
+JWbound_proj <- spTransform(JWboundary,
+                              crs(dem))
+JWsens_proj <- spTransform(JW_sensors,
+                            crs(dem))
+
+
+## crop and mask
+dem_crop <- crop(dem, extent(JWbound_proj))
+JW_dem<- mask(dem_crop, JWbound_proj)
+
+## Check that it worked
+plot(JW_dem)
+plot(JWsens_proj, add=TRUE)
+
+hist(JW_dem, main="Distribution of elevation values", 
+     col= "purple", 
+     maxpixels=22000000)
+
+contour <- contour(JW_dem, add=TRUE)
+plot(JW_dem)+
+  
+
+# Plot the contours using spplot:
+  spplot(contour_output["Elevation"],contour=TRUE)
