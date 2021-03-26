12bb4610cb1d1dce710f13e5e22a7d8ee3292d1e
searsmg <megan.sears@colostate.edu>
2021-03-26 17:52:55 -0600

    working on things

--- HydroDays_2021/BM_melt2020.R
+++ HydroDays_2021/BM_melt2020.R
@@ -10,20 +10,13 @@ rm(list=ls())
 
 ## data prep
 
-#sensor data from install to Jan 2021
-BM <- read.csv("C:/Users/sears/Documents/Repos/CSU-MS-Research/HydroDays_2021/BMdata.csv") %>% 
-  mutate(date = mdy(date)) %>% 
-  mutate(time = hm(time))
-
-#subset for melt 2020 season (May 1 [install] to July 1 2020)
-melt20 <- BM %>%
-  filter(date >= as.Date("2020-05-01")) %>%
-  filter(date <= as.Date("2020-07-01"))
-
 #read in elev data
 elev <- read.csv("C:/Users/sears/Documents/Repos/CSU-MS-Research/HydroDays_2021/elev_data.csv")
 
+slope <- read.csv("C:/Users/sears/Documents/Repos/CSU-MS-Research/HydroDays_2021/melt2020_slope.csv")%>%
+  mutate(date.time = mdy(Datetime)) %>% 
+  mutate(time = hms(time))
 
 
-
-melt20$mean <- rowMeans(melt20[,3:16], na.rm=TRUE)
+slope$time0 <- 0
+ggplot(slope)+ geom_raster(aes(x=date.time, y=time, fill=slope))
--- JW_Spatial/JW_spatial.R
+++ JW_Spatial/JW_spatial.R
@@ -16,28 +16,29 @@ rm(list = ls())
 
   
 # add DEM:
-dem  <- raster("DEM/ned30m40105.tif")
+dem  <- raster("JW_Spatial/DEM/ned30m40105.tif")
 
 # the distribution of values in the raster
-hist(dem, main="Distribution of elevation values", 
-     col= "purple", 
-     maxpixels=22000000)
+#hist(dem, main="Distribution of elevation values", 
+#     col= "purple", 
+#     maxpixels=22000000)
 
-plot(dem, 
-     main="DEM JW (need to clip)") # add title with main
-cropbox1 <- drawExtent()
+#plot(dem, 
+#     main="DEM JW (need to clip)") # add title with main
+#cropbox1 <- drawExtent()
 
-image(dem)
+#image(dem)
 
-test <- delineateWatershed(xlocation = -105.882005, ylocation = 40.540477, crs = 4326,
-                          includeparameters = "true")
+#test <- delineateWatershed(xlocation = -105.882005, ylocation = 40.540477, crs = 4326,
+#                          includeparameters = "true")
 
-leafletWatershed(JWboundary)
+#leafletWatershed(JWboundary)
 
-writeShapefile(watershed = test, layer = "layer_name", dir = "ws1_shp", what = "boundary")
+#writeShapefile(watershed = test, layer = "layer_name", dir = "ws1_shp", what = "boundary")
 
-JWboundary <- readOGR(dsn = "ws1_shp", layer = "layer_name")
-JW_sensors <- readOGR(dsn="sensors", layer="BMsensors_all")
+## add in sensor and watershed boundary - NOTE to add outlet sp
+JWboundary <- readOGR(dsn = "JW_Spatial/JW_boundary", layer = "globalwatershed")
+JW_sensors <- readOGR(dsn="JW_Spatial/SensorLocations", layer="BMsensors_all")
 
 JWbound_proj <- spTransform(JWboundary,
                               crs(dem))
@@ -53,13 +54,30 @@ JW_dem<- mask(dem_crop, JWbound_proj)
 plot(JW_dem)
 plot(JWsens_proj, add=TRUE)
 
-hist(JW_dem, main="Distribution of elevation values", 
-     col= "purple", 
-     maxpixels=22000000)
 
-contour <- contour(JW_dem, add=TRUE)
-plot(JW_dem)+
-  
+hist(JW_dem,
+     main=NULL,
+     breaks=7,
+     maxpixels=22000000, ylab="Frequency", xlab="Elevation (m)")
 
-# Plot the contours using spplot:
-  spplot(contour_output["Elevation"],contour=TRUE)
+#Plot size
+PlotWidth = 15
+PlotHeight = 9
+
+# use this plot format on remaining assignments - tweaked to remove grid lines and gray background
+PlotFormat = theme(axis.text=element_text(size=20),
+                   axis.title.x=element_text(size=24, hjust=0.5, margin=margin(t=20, r=20, b=20, l=20)),              
+                   axis.title.y=element_text(size=24, vjust=0.5,  margin=margin(t=20, r=20, b=20, l=20)),              
+                   plot.title=element_text(size=26,face="bold",hjust=0.5, margin=margin(t=20, r=20, b=20, l=20)),      
+                   legend.title=element_blank(),                                                                    
+                   legend.text=element_text(size=20),                                                                   
+                   legend.position = "bottom", 
+                   panel.grid.major = element_blank(), 
+                   panel.grid.minor = element_blank(),
+                   panel.background = element_blank(), 
+                   axis.line = element_line(colour = "black"))
+
+
+ggplot(demdata)+geom_histogram(aes(x=ned30m40105), fill="black", color="red", binwidth = 120) + PlotFormat
+
+sum(JW_dem)
