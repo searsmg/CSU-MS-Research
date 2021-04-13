424bb66189363da8ed6d6c31b6f5e3a6d2d3f304
searsmg <megan.sears@colostate.edu>
2021-04-12 21:30:37 -0600

    redoing BM data wrangle

--- BM_DataWrangle/BlueMaestro_DataWrangle2.R
+++ BM_DataWrangle/BlueMaestro_DataWrangle2.R
@@ -1,24 +1,26 @@
+
+
 library(dplyr)
 library(lubridate)
 library(tidyverse)
+library(stringr)
 
 rm(list = ls()) 
 
-setwd("C:/Users/sears/Documents/Research/Snow_Hydro_Research/BM Sensors/Field_Data_011821/Processed/")
+setwd("C:/Users/sears/Documents/Research/Snow_Hydro_Research/BM Sensors/Field_Data_041021/Processed/")
 
-BM_reformat <- read.csv("C:/Users/sears/Documents/Research/Snow_Hydro_Research/BM Sensors/Field_Data_011821/Processed/BM_use.csv")
+#read in all files. remove top two rows and several columns
+temp = list.files(pattern="")
+for (i in 1:length(temp)) assign(temp[i], read.csv(temp[i], 
+                                                   skip=2, header=TRUE, stringsAsFactors = FALSE))
 
-str(BM_reformat) #check date formats
+addID <- function(dfList){
+  for(i in 1:length(dfList)){
+  df$ID = deparse(substitute(df))
+  df$ID = str_remove(df$ID, ".csv")
+  }
+  return(df)
+}
 
-#get all date and time formats into POSIXct
-BM_reformat <- BM_reformat %>%
-  mutate(datetime = mdy_hm(datetime)) %>%
-  mutate(date = mdy(date)) %>%
-  mutate(sunrise=mdy_hm(sunrise)) %>%
-  mutate(solarNoon=mdy_hm(solarNoon)) %>%
-  mutate(sunset=mdy_hm(sunset))
-         
-test <- BM_reformat %>%
-  spread(ID, temperature)
-  
-  
+HD1 <- addID(HD1.csv)
+
