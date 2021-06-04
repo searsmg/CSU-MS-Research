88ba8d146ddd9d3596d20baf564e466b50b213a3
searsmg <megan.sears@colostate.edu>
2021-06-04 16:04:36 -0600

    quick stats for study site section of thesis

--- JW_Bulk_Dens_SWE/quick_JWsnotel_WY21dens.R
+++ JW_Bulk_Dens_SWE/quick_JWsnotel_WY21dens.R
@@ -4,6 +4,7 @@ library(RNRCS) # pull SNOTEL data
 library(lubridate)
 library(dplyr)
 library(ggplot2)
+library(dataRetrieval)
 
 #download Joe Wright SNOTEL data from WY 2010 to present and add as a df. Also, get date into correct format.
 JW21_sno <- grabNRCS.data(network = "SNTL", site_id = 551, timescale = "daily", DayBgn = '2020-10-01', DayEnd = '2021-06-03') %>%
@@ -16,4 +17,29 @@ PLOT = "JW SNOTEL dens over time"
 ggplot(data = subset(JW21_sno, !is.na(dens)), aes(x=Date, y=dens))+
   geom_line() + scale_x_date(date_breaks = "1 month")
 
-#test
+#pull all JW SNOTEL data 
+JWall_sno <- grabNRCS.data(network = "SNTL", site_id = 551, timescale = "daily", DayBgn = '1978-10-01', DayEnd = '2020-09-30') %>%
+  mutate(Date = ymd(Date))
+
+JWall_sno <- addWaterYear(JWall_sno)
+
+#some random stats for 2.1 - peak SWE and total precip accum
+JW_yravg <- JWall_sno %>%
+  group_by(waterYear) %>%
+  summarize(max_accumprecip = max(Precipitation.Accumulation..in..Start.of.Day.Values),
+            maxSWE = max(Snow.Water.Equivalent..in..Start.of.Day.Values)) %>% 
+  mutate(max_accumeprecip = max_accumprecip*2.54,
+         maxSWE=maxSWE*2.54)
+
+mean(JW_yravg$maxSWE, na.rm=TRUE)
+mean(JW_yravg$max_accumprecip, na.rm=TRUE)
+
+JW_peakSWEdate <- JWall_sno %>%
+  group_by(waterYear) %>%
+  slice(which.max(Snow.Water.Equivalent..in..Start.of.Day.Values)) %>% 
+  filter(waterYear >= 1980)
+
+JW_peakSWEdate$moday <- format(JW_peakSWEdate$Date,"%j")
+JW_peakSWEdate$moday <- as.numeric(JW_peakSWEdate$moday)
+
+mean(JW_peakSWEdate$moday)
