68790821480d84f4ec66db5a32aba9fa39c9c714
searsmg <megan.sears@colostate.edu>
2021-08-10 18:31:00 -0600

    updated wd

--- Melt2021/Outliers2021.R
+++ Melt2021/Outliers2021.R
@@ -11,7 +11,7 @@ rm(list = ls())
 ############DATA PREP#############
 
 #set working directory and csv file
-setwd("C:/Users/sears/Documents/Research/Snow_Hydro_Research/Thesis/Data/Air Temp") #update wd
+setwd("C:/Users/sears/Documents/Research/Snow_Hydro_Research/Thesis/Data/Air Temp/For R") #update wd
 
 outlier21 <- read.csv(file="AirT_outliers_21.csv", header=TRUE)
 outlier21 <- subset(outlier21, select = -X)
