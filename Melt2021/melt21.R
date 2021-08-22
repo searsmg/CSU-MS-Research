256461197df044825bdab1ca2d0de6c966da0bbd
searsmg <megan.sears@colostate.edu>
2021-08-22 12:24:41 -0600

    update minute from 01 to 00 on bandT, added rad to bandT, create a DF for each band w/ temp and rad

--- Melt2021/melt21.R
+++ Melt2021/melt21.R
@@ -29,6 +29,9 @@ temp_elev_21 <- read.csv(file="Melt21_elev_noOUT.csv", header=TRUE) %>%
 bandT <- read.csv(file="bands21_interpol.csv", header=TRUE) %>%
   mutate(Datetime = mdy_hm(Datetime))
 
+#get rid of the minute on date time (e.g., 14:01 - want 14:00)
+minute(bandT$Datetime) <- 0
+
 #start working on rad - this is 10 min data and hourly avg is every 7 rows
 rad21 <- read.csv(file="C:/Users/sears/Documents/Research/Snow_Hydro_Research/Thesis/Data/Radiation/For R/Rad_melt21.csv", 
                   header=TRUE) %>%
@@ -39,6 +42,50 @@ rad21 <- rad21 %>%
   filter(minute(Datetime) == 0) %>%
   filter(row_number() %% 2 != 1) ## Delete odd-rows
 
-#break bands to own df
-  
+#add rad to bandT
+bandT_rad <- merge(bandT, rad21, by = "Datetime")
+
+#now break each band out so we can calculate rad and melt
+b1 <- bandT_rad %>%
+  select(Datetime, B1, Swin, Swout, Lwin, Lwout)
+
+b2 <- bandT_rad %>%
+  select(Datetime, B2, Swin, Swout, Lwin, Lwout)
+
+b3 <- bandT_rad %>%
+  select(Datetime, B3, Swin, Swout, Lwin, Lwout)
+
+b4 <- bandT_rad %>%
+  select(Datetime, B4, Swin, Swout, Lwin, Lwout)
+
+b5 <- bandT_rad %>%
+  select(Datetime, B5, Swin, Swout, Lwin, Lwout)
+
+b6 <- bandT_rad %>%
+  select(Datetime, B6, Swin, Swout, Lwin, Lwout)
+
+b7 <- bandT_rad %>%
+  select(Datetime, B7, Swin, Swout, Lwin, Lwout)
+
+b8 <- bandT_rad %>%
+  select(Datetime, B8, Swin, Swout, Lwin, Lwout)
+
+b9 <- bandT_rad %>%
+  select(Datetime, B9, Swin, Swout, Lwin, Lwout)
+
+b10 <- bandT_rad %>%
+  select(Datetime, B10, Swin, Swout, Lwin, Lwout)
+
+b11 <- bandT_rad %>%
+  select(Datetime, B11, Swin, Swout, Lwin, Lwout)
+
+b12 <- bandT_rad %>%
+  select(Datetime, B12, Swin, Swout, Lwin, Lwout)
+
+b13 <- bandT_rad %>%
+  select(Datetime, B13, Swin, Swout, Lwin, Lwout)
+
+b14 <- bandT_rad %>%
+  select(Datetime, B14, Swin, Swout, Lwin, Lwout)
+
 
