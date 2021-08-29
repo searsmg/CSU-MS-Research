d993e0f3644216dd69d79d405aecc46cedccf16e
searsmg <megan.sears@colostate.edu>
2021-08-28 18:58:27 -0600

    converting units

--- Melt2021/MFs.R
+++ Melt2021/MFs.R
@@ -1,1 +1,22 @@
 # calibrating MELT FACTORS for melt 2021
+
+library(ggplot2)
+library(lubridate)
+library(dplyr)
+
+rm(list = ls()) 
+
+#set working directory and csv file
+setwd("C:/Users/sears/Documents/Research/Snow_Hydro_Research/Thesis/Data/SNOTEL")
+
+sno21 <- read.csv("JW21.csv", header=TRUE) %>%
+  mutate(Date = mdy_hm(Date))
+
+#convert to metric units and get rid of the others
+sno21 <- sno21 %>%
+  mutate(Ta_C = (AirT_F-32)*(5/9),
+         PrecipAcum_mm = PrecipAcum_in*25.4,
+         Sd_mm = SnowDepth_in*25.4,
+         SWE_mm = SWE_in*25.4) %>%
+  select(-c(AirT_F, PrecipAcum_in, SnowDepth_in, SWE_in))
+
