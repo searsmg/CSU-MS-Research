0838f3ba069e245affe3bd11c0b3fb41a3ddfd75
searsmg <megan.sears@colostate.edu>
2021-08-30 18:19:00 -0600

    added in function and params. need to figure out observed melt and minimizing sum of least squares for params

--- Melt2021/MFs.R
+++ Melt2021/MFs.R
@@ -18,5 +18,40 @@ sno21 <- sno21 %>%
          PrecipAcum_mm = PrecipAcum_in*25.4,
          Sd_mm = SnowDepth_in*25.4,
          SWE_mm = SWE_in*25.4) %>%
-  select(-c(AirT_F, PrecipAcum_in, SnowDepth_in, SWE_in))
+  select(-c(AirT_F, PrecipAcum_in, SnowDepth_in, SWE_in)) %>%
+  mutate(d_SWE = lag(SWE_mm, default=first(SWE_mm)) - SWE_mm)
+
+#find observed melt
+
+
+# function for melt
+melt <- function(MFt, Ta, Tt, MFh, NR){
+  melt_mm <- MFt*(Ta-Tt)+MFh*(NR)
+  return(melt_mm)
+}
+
+#bring in rad data using CAIC (brought in from melt21.R)
+rad21 <- read.csv(file="C:/Users/sears/Documents/Research/Snow_Hydro_Research/Thesis/Data/Radiation/For R/Rad_melt21.csv", 
+                  header=TRUE) %>%
+  mutate(Datetime = mdy_hm(Datetime))
+
+#get hourly rad data
+rad21 <- rad21 %>%
+  filter(minute(Datetime) == 0) %>%
+  filter(row_number() %% 2 != 1) %>% #delete odd rows
+  rename(Date = Datetime)
+
+sno21 <- merge(sno21, rad21, by="Date")
+
+sno21 <- sno21 %>%
+  mutate(NR = (Swin-Swout)+(Lwin-Lwout))
+
+#define constants
+MFt <- 1
+Tt <- 1
+MFh <- 1
+
+# modelet melt
+sno21$melt_mod <- melt(MFt, sno21$Ta_C, Tt, MFh, sno21$NR)
+
 
