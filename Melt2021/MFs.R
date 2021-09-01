49b748e715d3d7bf90c65a11b3fb6a4da18ee3bf
searsmg <megan.sears@colostate.edu>
2021-09-01 16:34:14 -0600

    figured out optim. now need to get actual observed melt

--- Melt2021/MFs.R
+++ Melt2021/MFs.R
@@ -3,11 +3,16 @@
 library(ggplot2)
 library(lubridate)
 library(dplyr)
+library(plotly)
+library(optimr)
 
 rm(list = ls()) 
 
-#set working directory and csv file
+#set working directory
 setwd("C:/Users/sears/Documents/Research/Snow_Hydro_Research/Thesis/Data/SNOTEL")
+
+######################################################
+#first find MFs using SNOTEL data (practice)
 
 sno21 <- read.csv("JW21.csv", header=TRUE) %>%
   mutate(Date = mdy_hm(Date))
@@ -20,15 +25,6 @@ sno21 <- sno21 %>%
          SWE_mm = SWE_in*25.4) %>%
   select(-c(AirT_F, PrecipAcum_in, SnowDepth_in, SWE_in)) %>%
   mutate(d_SWE = lag(SWE_mm, default=first(SWE_mm)) - SWE_mm)
-
-#find observed melt
-
-
-# function for melt
-melt <- function(MFt, Ta, Tt, MFh, NR){
-  melt_mm <- MFt*(Ta-Tt)+MFh*(NR)
-  return(melt_mm)
-}
 
 #bring in rad data using CAIC (brought in from melt21.R)
 rad21 <- read.csv(file="C:/Users/sears/Documents/Research/Snow_Hydro_Research/Thesis/Data/Radiation/For R/Rad_melt21.csv", 
@@ -46,12 +42,26 @@ sno21 <- merge(sno21, rad21, by="Date")
 sno21 <- sno21 %>%
   mutate(NR = (Swin-Swout)+(Lwin-Lwout))
 
-#define constants
-MFt <- 1
-Tt <- 1
-MFh <- 1
+######################################################
+#optimize 
 
-# modelet melt
-sno21$melt_mod <- melt(MFt, sno21$Ta_C, Tt, MFh, sno21$NR)
+melt <- function(data, par){
+  with(data, sum(((par[1]*(Ta_C-0)+par[2]*NR)-d_SWE)^2))
+}
+
+MFs <- optim(par=c(0,0), fn=melt, data=sno21)
+ 
+######################################################
+#see how SNOTEL looks with new MFs
+sno21 <- sno21 %>%
+  mutate(melt_mod = 0.0277317240*(Ta_C-0)+-0.0009764329*(NR))
+
+compare <- ggplot(sno21)+geom_line(aes(x=Date, y=melt_mod))+geom_point(aes(x=Date, y=d_SWE))
+
+ggplotly(compare)
+
+######################################################
+######################################################
+#Find MFs using snow pit from melt observed
 
 
