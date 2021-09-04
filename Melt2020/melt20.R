87b7bc54afe26dd8696efd19fcb938116587c47d
searsmg <megan.sears@colostate.edu>
2021-09-04 17:25:26 -0600

    making figures for temp data

---
+++ Melt2020/melt20.R
@@ -0,0 +1,203 @@
+### Script to calculate Melt 20 data ###
+
+library(ggplot2)
+library(lubridate)
+library(dplyr)
+library(plotly)
+library(tidyverse)
+
+rm(list = ls()) 
+
+#set working directory and csv file
+setwd("C:/Users/sears/Documents/Research/Snow_Hydro_Research/Thesis/Data/Air Temp/For R/2020")
+
+##########################################################################
+#### PROCESSING ####
+
+#temp, elevation, and elevation band data
+temp_elev_20 <- read.csv(file="AirT_elev20_noOUT.csv", header=TRUE) %>%
+  mutate(Datetime = mdy_hm(Datetime))
+
+RH_dewpt <- read.csv(file="RH_dewpt_all.csv", header=TRUE) %>%
+  mutate(Datetime = mdy_hm(Datetime))
+
+all20 <- merge(temp_elev_20, RH_dewpt, by=c("Datetime", "ID"))
+
+#get a temp for each elev band (grouping bands and getting avg temp)
+bands20 <- all20 %>%
+  group_by(Datetime, Band) %>%
+  summarize(bandT = mean(AirT_C, na.rm=TRUE),
+            bandRH = mean(humidity),
+            banddewpt = mean(dewpoint))
+
+bandT_long <- bands20 %>%
+  select(Datetime, bandT, Band)
+  pivot_wider(names_from = Band, values_from = bandT)
+
+#write.csv(bandT_long, "bandT.csv")
+
+bandRH_long <- bands20 %>%
+  select(Datetime, bandRH,Band) %>%
+  pivot_wider(names_from = Band, values_from=bandRH)
+
+#write.csv(bandRH_long, "bandRH.csv")
+
+###################################################
+#bring band T back in - have all Ta for melt 21
+#bandT <- read.csv(file="bands21_9.csv", header=TRUE) %>%
+#  mutate(Datetime = mdy_hm(Datetime))
+
+#bring RH back in
+#bandRH <- read.csv(file="bandRH.csv", header=TRUE) %>%
+#  mutate(Datetime = mdy_hm(Datetime))
+
+#get rid of the minute on date time (e.g., 14:01 - want 14:00)
+minute(bandT_long$Datetime) <- 0
+minute(bandRH_long$Datetime) <- 0
+
+#start working on rad - this is 10 min data and hourly avg is every 7 rows
+rad21 <- read.csv(file="C:/Users/sears/Documents/Research/Snow_Hydro_Research/Thesis/Data/Radiation/For R/Rad_melt21.csv", 
+                  header=TRUE) %>%
+  mutate(Datetime = mdy_hm(Datetime))
+
+#get hourly rad data
+rad21 <- rad21 %>%
+  filter(minute(Datetime) == 0) %>%
+  filter(row_number() %% 2 != 1) ## Delete odd-rows
+
+#add rad to bandT
+bandT_rad <- merge(bandT, rad21, by = "Datetime")
+
+#add RH to bandT+rad
+band_all <- merge(bandT_rad, bandRH, by="Datetime")
+
+#now break each band out so we can calculate rad and melt
+b1_2 <- band_all %>%
+  select(Datetime, b1_2, Swin, Swout, Lwin, Lwout, b1_2_RH)
+
+b3 <- band_all %>%
+  select(Datetime, b3, Swin, Swout, Lwin, Lwout, b3_RH)
+
+b4 <- band_all %>%
+  select(Datetime, b4, Swin, Swout, Lwin, Lwout, b4_RH)
+
+b5 <- band_all %>%
+  select(Datetime, b5, Swin, Swout, Lwin, Lwout, b5_RH)
+
+b6 <- band_all %>%
+  select(Datetime, b6, Swin, Swout, Lwin, Lwout, b6_RH)
+
+b7 <- band_all %>%
+  select(Datetime, b7, Swin, Swout, Lwin, Lwout, b7_RH)
+
+b8 <- band_all %>%
+  select(Datetime, b8, Swin, Swout, Lwin, Lwout, b8_RH)
+
+b9 <- band_all %>%
+  select(Datetime, b9, Swin, Swout, Lwin, Lwout, b9_RH)
+
+b10_14 <- band_all %>%
+  select(Datetime, b10_14, Swin, Swout, Lwin, Lwout, b10_RH)
+
+
+#### PROCESSING DONE ####
+##########################################################################
+
+## DON'T NEED TO RUN ANY OF THE ABOVE LINES AS LONG AS RDATA FILE IS READ IN
+load("C:/Users/sears/Documents/Repos/CSU-MS-Research/Melt2021/DFs.Rdata")
+
+## for each band compute LWin
+# first find sat vapor press (ea), then back out Cc. fix Cc for >1 or <1, then calc LW in for all bands
+
+#stefan boltz constant
+stef <- 5.67 * 10^-8
+
+b1_2 <- b1_2 %>%
+  mutate(esat = (6.112*exp((17.62*b1_2)/(243.12+b1_2)))) %>%
+  mutate(ea = (b1_2_RH * esat)/100) %>%
+  mutate(Cc_pt1 = Lwin/((stef)*(b1_2+273.15)^4)) %>%
+  mutate(Cc_pt2 = Cc_pt1 /(0.53+(0.065*ea))) %>%
+  mutate(Cc = (Cc_pt2 -1)/0.4) %>%
+  mutate(Cc_fix = if_else(Cc<0,0,if_else(Cc>1,1,Cc))) %>%
+  mutate(Lwin_fix = (0.53+(0.065*ea))*(1+(0.4*Cc_fix))*(5.67*10^-8)*((b1_2+273.15)^4)) %>%
+  select(-c(Cc_pt1, Cc_pt2, Cc))
+
+b3 <- b3 %>%
+  mutate(esat = (6.112*exp((17.62*b3)/(243.12+b3)))) %>%
+  mutate(ea = (b3_RH * esat)/100) %>%
+  mutate(Cc_pt1 = Lwin/((stef)*(b3+273.15)^4)) %>%
+  mutate(Cc_pt2 = Cc_pt1 /(0.53+(0.065*ea))) %>%
+  mutate(Cc = (Cc_pt2 -1)/0.4) %>%
+  mutate(Cc_fix = if_else(Cc<0,0,if_else(Cc>1,1,Cc))) %>%
+  mutate(Lwin_fix = (0.53+(0.065*ea))*(1+(0.4*Cc_fix))*(5.67*10^-8)*((b3+273.15)^4)) %>%
+  select(-c(Cc_pt1, Cc_pt2, Cc))
+
+b4 <- b4 %>%
+  mutate(esat = (6.112*exp((17.62*b4)/(243.12+b4)))) %>%
+  mutate(ea = (b4_RH * esat)/100) %>%
+  mutate(Cc_pt1 = Lwin/((stef)*(b4+273.15)^4)) %>%
+  mutate(Cc_pt2 = Cc_pt1 /(0.53+(0.065*ea))) %>%
+  mutate(Cc = (Cc_pt2 -1)/0.4) %>%
+  mutate(Cc_fix = if_else(Cc<0,0,if_else(Cc>1,1,Cc))) %>%
+  mutate(Lwin_fix = (0.53+(0.065*ea))*(1+(0.4*Cc_fix))*(5.67*10^-8)*((b4+273.15)^4)) %>%
+  select(-c(Cc_pt1, Cc_pt2, Cc))
+
+b5 <- b5 %>%
+  mutate(esat = (6.112*exp((17.62*b5)/(243.12+b5)))) %>%
+  mutate(ea = (b5_RH * esat)/100) %>%
+  mutate(Cc_pt1 = Lwin/((stef)*(b5+273.15)^4)) %>%
+  mutate(Cc_pt2 = Cc_pt1 /(0.53+(0.065*ea))) %>%
+  mutate(Cc = (Cc_pt2 -1)/0.4) %>%
+  mutate(Cc_fix = if_else(Cc<0,0,if_else(Cc>1,1,Cc))) %>%
+  mutate(Lwin_fix = (0.53+(0.065*ea))*(1+(0.4*Cc_fix))*(5.67*10^-8)*((b5+273.15)^4)) %>%
+  select(-c(Cc_pt1, Cc_pt2, Cc))
+
+b6 <- b6 %>%
+  mutate(esat = (6.112*exp((17.62*b6)/(243.12+b6)))) %>%
+  mutate(ea = (b6_RH * esat)/100) %>%
+  mutate(Cc_pt1 = Lwin/((stef)*(b6+273.15)^4)) %>%
+  mutate(Cc_pt2 = Cc_pt1 /(0.53+(0.065*ea))) %>%
+  mutate(Cc = (Cc_pt2 -1)/0.4) %>%
+  mutate(Cc_fix = if_else(Cc<0,0,if_else(Cc>1,1,Cc))) %>%
+  mutate(Lwin_fix = (0.53+(0.065*ea))*(1+(0.4*Cc_fix))*(5.67*10^-8)*((b6+273.15)^4)) %>%
+  select(-c(Cc_pt1, Cc_pt2, Cc))
+
+b7 <- b7 %>%
+  mutate(esat = (6.112*exp((17.62*b7)/(243.12+b7)))) %>%
+  mutate(ea = (b7_RH * esat)/100) %>%
+  mutate(Cc_pt1 = Lwin/((stef)*(b7+273.15)^4)) %>%
+  mutate(Cc_pt2 = Cc_pt1 /(0.53+(0.065*ea))) %>%
+  mutate(Cc = (Cc_pt2 -1)/0.4) %>%
+  mutate(Cc_fix = if_else(Cc<0,0,if_else(Cc>1,1,Cc))) %>%
+  mutate(Lwin_fix = (0.53+(0.065*ea))*(1+(0.4*Cc_fix))*(5.67*10^-8)*((b7+273.15)^4)) %>%
+  select(-c(Cc_pt1, Cc_pt2, Cc))
+
+b8 <- b8 %>%
+  mutate(esat = (6.112*exp((17.62*b8)/(243.12+b8)))) %>%
+  mutate(ea = (b8_RH * esat)/100) %>%
+  mutate(Cc_pt1 = Lwin/((stef)*(b8+273.15)^4)) %>%
+  mutate(Cc_pt2 = Cc_pt1 /(0.53+(0.065*ea))) %>%
+  mutate(Cc = (Cc_pt2 -1)/0.4) %>%
+  mutate(Cc_fix = if_else(Cc<0,0,if_else(Cc>1,1,Cc))) %>%
+  mutate(Lwin_fix = (0.53+(0.065*ea))*(1+(0.4*Cc_fix))*(5.67*10^-8)*((b8+273.15)^4)) %>%
+  select(-c(Cc_pt1, Cc_pt2, Cc))
+
+b9 <- b9 %>%
+  mutate(esat = (6.112*exp((17.62*b9)/(243.12+b9)))) %>%
+  mutate(ea = (b9_RH * esat)/100) %>%
+  mutate(Cc_pt1 = Lwin/((stef)*(b9+273.15)^4)) %>%
+  mutate(Cc_pt2 = Cc_pt1 /(0.53+(0.065*ea))) %>%
+  mutate(Cc = (Cc_pt2 -1)/0.4) %>%
+  mutate(Cc_fix = if_else(Cc<0,0,if_else(Cc>1,1,Cc))) %>%
+  mutate(Lwin_fix = (0.53+(0.065*ea))*(1+(0.4*Cc_fix))*(5.67*10^-8)*((b9+273.15)^4)) %>%
+  select(-c(Cc_pt1, Cc_pt2, Cc))
+
+b10_14 <- b10_14 %>%
+  mutate(esat = (6.112*exp((17.62*b10_14)/(243.12+b10_14)))) %>%
+  mutate(ea = (b10_RH * esat)/100) %>%
+  mutate(Cc_pt1 = Lwin/((stef)*(b10_14+273.15)^4)) %>%
+  mutate(Cc_pt2 = Cc_pt1 /(0.53+(0.065*ea))) %>%
+  mutate(Cc = (Cc_pt2 -1)/0.4) %>%
+  mutate(Cc_fix = if_else(Cc<0,0,if_else(Cc>1,1,Cc))) %>%
+  mutate(Lwin_fix = (0.53+(0.065*ea))*(1+(0.4*Cc_fix))*(5.67*10^-8)*((b10_14+273.15)^4)) %>%
+  select(-c(Cc_pt1, Cc_pt2, Cc))
---
+++ Temp_melt20&21/Temps_20and21.R
@@ -0,0 +1,123 @@
+# looking at air temps for melt season 2020 and 2021
+
+library(ggplot2)
+library(lubridate)
+library(dplyr)
+library(plotly)
+library(gridExtra)
+library(scales)
+library(RColorBrewer)
+
+rm(list = ls()) 
+
+#set working directory and csv file
+setwd("C:/Users/sears/Documents/Research/Snow_Hydro_Research/Thesis/Data/Air Temp")
+
+#bring in 20 and 21 temp datasets
+T20 <- read.csv(file = "C:/Users/sears/Documents/Research/Snow_Hydro_Research/Thesis/Data/Air Temp/For R/2020/AirT_elev20_noOUT.csv",
+                header=TRUE) %>%
+  mutate(Datetime = mdy_hm(Datetime))
+
+T21 <- read.csv(file="C:/Users/sears/Documents/Research/Snow_Hydro_Research/Thesis/Data/Air Temp/For R/2021/Melt21_elev_noOUT.csv",
+                header=TRUE) %>%
+  mutate(Datetime = mdy_hm(Datetime))
+
+#remvoe the 01 from the minutes
+minute(T20$Datetime) <- 0
+minute(T21$Datetime) <- 0
+
+PLOT = "Temp Plot"
+t20 <- ggplot() + geom_point(data=T20, aes(x=Elevation, y=AirT_C, colour=ID))
+t20
+t21 <- ggplot() + geom_point(data=T21, aes(x=Elevation, y=AirT_C,colour=ID))
+grid.arrange(t20,t21, ncol=2)
+
+#ggsave(paste(PLOT,".png",sep=""), width = 15, height = 9)
+
+##########################
+
+T20_long <- T20 %>%
+  select(Datetime, ID, AirT_C) %>%
+  pivot_wider(names_from = ID, values_from=AirT_C)
+
+#write.csv(T20_long, "T20.csv")
+
+T21_long <- T21 %>%
+  select(Datetime, ID, AirT_C) %>%
+  pivot_wider(names_from = ID, values_from=AirT_C)
+
+#write.csv(T21_long, "T21.csv")
+
+####
+T20_slope <- read.csv(file = "C:/Users/sears/Documents/Research/Snow_Hydro_Research/Thesis/Data/Air Temp/T20_nstge.csv",
+                header=TRUE) %>%
+  mutate(Datetime = mdy_hm(Datetime))
+
+T21_slope <- read.csv(file = "C:/Users/sears/Documents/Research/Snow_Hydro_Research/Thesis/Data/Air Temp/T21_nstge.csv",
+                      header=TRUE) %>%
+  mutate(Datetime = mdy_hm(Datetime))
+
+#########
+
+slope20 <- ggplot() + geom_point(data=T20_slope, aes(x=Datetime, y=Slope_degCkm)) +
+  ylim(-30,30) +
+  geom_hline(yintercept=6.5) +
+  scale_x_datetime(date_labels = "%b") +
+  PlotFormat + 
+  labs(x="Date", y="NSTGE (deg C/km)")
+slope20
+
+slope21 <- ggplot() + geom_point(data=T21_slope, aes(x=Datetime, y=Slope_degCkm)) +
+  geom_hline(yintercept=6.5)+
+  scale_x_datetime(date_labels = "%b") +
+  PlotFormat + 
+  labs(x="Date", y="NSTGE (deg C/km)")
+slope21
+
+grid.arrange(slope20, slope21, ncol=2)
+###############
+
+T20_slope <- T20_slope %>%
+  mutate(year = "2020")
+
+T21_slope <- T21_slope %>%
+  mutate(year = "2021")
+
+slope <- rbind(T20_slope, T21_slope)
+
+PLOT = "NSTGE_20&21"
+ggplot(slope, aes(x=Datetime, y=Slope_degCkm)) +
+  geom_point() + 
+  geom_hline(yintercept=-6.5, size=1, color="Red") +
+  ylim(-30,30) +
+  labs(x= "Date", y="NSTGE (deg C/km)") +
+  scale_x_datetime(date_labels = "%b", date_break = "1 month") +
+  facet_wrap(~year, scales="free_x")
+
+ggsave(paste(PLOT,".png",sep=""), width = 15, height = 9)
+
+##########################
+
+slope_edit <- slope %>%
+  mutate(hour = hour(Datetime)) %>%
+  mutate(doy = yday(Datetime)) %>%
+  filter(Slope_degCkm < 40) %>%
+  filter(Slope_degCkm > -32)
+
+PLOT="heatmap_slope"
+slope <- ggplot(slope_edit, aes(x=doy, y=hour, fill=Slope_degCkm)) +
+  geom_tile() + facet_grid(~year, scale="free_x") +
+  scale_fill_distiller(palette = 'RdYlBu')+
+  labs(fill="C/km")
+
+ggsave(paste(PLOT,".png",sep=""), width = 15, height = 9)
+
+PLOT = "heatmap_R2"
+r2 <- ggplot(slope_edit, aes(x=doy, y=hour, fill=R2)) +
+  geom_tile() + facet_grid(~year, scale="free_x") +
+  labs(fill="R^2") +
+  scale_fill_distiller(palette="Greens", trans="reverse")
+
+ggsave(paste(PLOT,".png",sep=""), width = 15, height = 9)
+
+grid.arrange(slope, r2, nrow=2)
