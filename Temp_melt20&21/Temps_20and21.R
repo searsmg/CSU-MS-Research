22fe0ccc017c0fe829fa104e5ac5cce9ce54c7ee
searsmg <megan.sears@colostate.edu>
2021-09-12 11:56:28 -0600

    updated format on air temp figs

--- Temp_melt20&21/Temps_20and21.R
+++ Temp_melt20&21/Temps_20and21.R
@@ -22,13 +22,13 @@ T21 <- read.csv(file="C:/Users/sears/Documents/Research/Snow_Hydro_Research/Thes
                 header=TRUE) %>%
   mutate(Datetime = mdy_hm(Datetime))
 ################################################
-PlotFormat = theme(axis.text=element_text(size=20),
-                   axis.title.x=element_text(size=24, hjust=0.5, margin=margin(t=20, r=20, b=20, l=20)),              
-                   axis.title.y=element_text(size=24, vjust=0.5,  margin=margin(t=20, r=20, b=20, l=20)),              
+PlotFormat = theme(axis.text=element_text(size=16),
+                   axis.title.x=element_text(size=18, hjust=0.5, margin=margin(t=20, r=20, b=20, l=20)),              
+                   axis.title.y=element_text(size=18, vjust=0.5,  margin=margin(t=20, r=20, b=20, l=20)),              
                    plot.title=element_text(size=26,face="bold",hjust=0.5, margin=margin(t=20, r=20, b=20, l=20)),      
-                   legend.title=element_blank(),                                                                    
-                   legend.text=element_text(size=20),                                                                   
-                   legend.position = "bottom", 
+                   legend.title=element_text(size=16),                                                                    
+                   legend.text=element_text(size=16),                                                                   
+                   legend.position = "right", 
                    panel.grid.major = element_blank(), 
                    panel.grid.minor = element_blank(),
                    panel.background = element_blank(), 
@@ -103,12 +103,13 @@ slope <- rbind(T20_slope, T21_slope)
 
 PLOT = "NSTGE_20&21"
 ggplot(slope, aes(x=Datetime, y=Slope_degCkm)) +
-  geom_point() + 
+  geom_point(aes(colour=R2)) + 
   geom_hline(yintercept=-6.5, size=1, color="Red") +
-  ylim(-30,30) +
-  labs(x= "Date", y="NSTGE (deg C/km)") +
+  ylim(-20,30) +
+  labs(x= "Date", y=expression("NSTGE " (degree*C/km)), color=expression(paste("R"^2))) +
   scale_x_datetime(date_labels = "%b", date_break = "1 month") +
-  facet_wrap(~year, scales="free_x") + PlotFormat
+  facet_wrap(~year, scales="free_x") + PlotFormat +
+  scale_color_gradient(low='grey', high='black')
 
 ggsave(paste(PLOT,".png",sep=""), width = 15, height = 9)
 
@@ -124,16 +125,20 @@ PLOT="heatmap_slope"
 slope <- ggplot(slope_edit, aes(x=doy, y=hour, fill=Slope_degCkm)) +
   geom_tile() + facet_grid(~year, scale="free_x") +
   scale_fill_distiller(palette = 'RdYlBu')+
-  labs(fill="C/km")
-
+  labs(fill=expression(degree*C/km), x="Day of Year", y="Hour") + PlotFormat +
+  scale_y_continuous(breaks=seq(0, 23, 4))
+slope
+  
 ggsave(paste(PLOT,".png",sep=""), width = 15, height = 9)
 
 PLOT = "heatmap_R2"
 r2 <- ggplot(slope_edit, aes(x=doy, y=hour, fill=R2)) +
   geom_tile() + facet_grid(~year, scale="free_x") +
-  labs(fill="R^2") +
-  scale_fill_distiller(palette="Greens", trans="reverse")
-
+  labs(fill=expression("R"^2), x="Day of year", y="Hour") +
+  scale_fill_gradientn(colors=brewer.pal(name="Greys", n=3)) +
+  scale_y_continuous(breaks=seq(0, 23, 4)) + PlotFormat
+r2
+  
 ggsave(paste(PLOT,".png",sep=""), width = 15, height = 9)
 
 grid.arrange(slope, r2, nrow=2)
