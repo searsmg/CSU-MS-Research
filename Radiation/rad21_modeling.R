c502beb50011af5bda73c572b7aaa1a15e0f6616
searsmg <megan.sears@colostate.edu>
2021-09-24 16:15:10 -0600

    working on albedo. is a mess rn

--- Radiation/rad21_modeling.R
+++ Radiation/rad21_modeling.R
@@ -8,6 +8,7 @@ library(dplyr)
 library(plotly)
 library(tidyverse)
 library(RNRCS)
+library(esquisse)
 
 #set working directory and csv file
 setwd("C:/Users/sears/Documents/Research/Snow_Hydro_Research/Thesis/Data/Radiation/For R")
@@ -21,7 +22,7 @@ JW21_hr <- grabNRCS.data(network = "SNTL", site_id = 551, timescale = "hourly", 
   mutate(Datetime = ymd_hm(Date))
 
 
-JW21_hr <- JW21_hr %>%
+JW21 <- JW21_hr %>%
   rename(Ta_F = Air.Temperature.Observed..degF., 
          precip_accum_in = Precipitation.Accumulation..in.,
          Sd_in = Snow.Depth..in.,
@@ -31,27 +32,27 @@ JW21_hr <- JW21_hr %>%
          Sd_mm = Sd_in*25.4,
          SWE_mm = SWE_in*25.4) %>%
   select(-c(Ta_F, precip_accum_in, Sd_in, SWE_in, Date)) %>%
-  mutate(precip_mm = PrecipAcum_mm - lag(PrecipAcum_mm))
+  mutate(precip_mm = PrecipAcum_mm - lag(PrecipAcum_mm)) %>%
+  drop_na()
 
-JW21_hr <- JW21_hr %>%
+JW21_hr_al <- JW21 %>%
   mutate(precip_mm = if_else(precip_mm<2.54,0,precip_mm)) %>%
   mutate(swe_mm = SWE_mm - lag(SWE_mm),
          sdepth = Sd_mm - lag(Sd_mm)) %>%
+  slice(-1) %>%
   mutate(freshsnow = if_else(sdepth > 10 & swe_mm > 2.54, precip_mm, 0)) %>%
-  mutate(soil_al = 0.2,
-         fresh_al = if_else(freshsnow > 0, 0.84, NA),
+  mutate(fresh_al = ifelse(freshsnow > 0, 0.84, NA),
          al_min = if_else(Ta_C < 0, 0.7, 0.5)) %>%
-  mutate(snowcum = cumsum(ifelse(is.na(freshsnow),0,freshsnow)) %>%
-  mutate(actual_al = ifelse(snowcum == 0, soil_al, NA)) %>%
-  mutate(snowstop = ifelse(snowcum == lag(snowcum), 0, 1)) %>%
-  mutate(snowstop = ifelse(is.na(snowstop), 0, snowstop)) %>%
+  mutate(snowcum = cumsum(if_else(is.na(freshsnow),0,freshsnow))) %>%
+  mutate(actual_al = ifelse(snowcum == 0, al_min, NA)) %>%
   mutate(actual_al = ifelse(freshsnow > 0, 0.84, actual_al))
   
-albedo = JW21_hr$actual_al
+albedo = JW21_hr_al$actual_al
 
-for(i in 2:nrow(JW21_hr)){
+for(i in 2:nrow(JW21_hr_al)){
   if(is.na(albedo[i])){
-    albedo[i] = ((albedo[i-1]-JW21_hr$al_min[i])*exp(-0.01)) + JW21_hr$al_min[i]
+    albedo[i] = ((albedo[i-1]-JW21_hr_al$al_min[i])*exp(-0.01)) + JW21_hr_al$al_min[i]
   }
 }
 
+ggplot(JW21_hr_al)+geom_line(aes(Datetime, actual_al))
