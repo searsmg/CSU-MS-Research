b676660fed0339af0351813c2f08d31944bb3997
searsmg <megan.sears@colostate.edu>
2021-10-24 15:21:19 -0600

    looking at full dataset for Ta and Td

---
+++ Temp_melt20&21/alltemps.R
@@ -0,0 +1,192 @@
+#all Ta and Td (looking at seasons)
+
+library(broom)
+library(tidyverse)
+library(ggplot2)
+library(lubridate)
+library(dplyr)
+library(plotly)
+library(gridExtra)
+library(scales)
+library(RColorBrewer)
+library(plotly)
+library(tidyr)
+library(tibble)
+
+rm(list = ls())
+
+##setup
+setwd("C:/Users/sears/Documents/Research/Snow_Hydro_Research/Thesis/Data/Air Temp/For R")
+
+PlotFormat = theme(axis.text=element_text(size=16, color="black"),
+                   axis.title.x=element_text(size=18, hjust=0.5, margin=margin(t=20, r=20, b=20, l=20), color="black"),              
+                   axis.title.y=element_text(size=18, vjust=0.5,  margin=margin(t=20, r=20, b=20, l=20), color="black"),              
+                   plot.title=element_text(size=26,face="bold",hjust=0.5, margin=margin(t=20, r=20, b=20, l=20)),      
+                   legend.title=element_text(size=14, color="black"),                                                                    
+                   legend.text=element_text(size=14, color="black"),                                                                   
+                   legend.position = "right", 
+                   panel.grid.major = element_blank(), 
+                   panel.grid.minor = element_blank(),
+                   panel.background = element_blank(), 
+                   #axis.line = element_line(colour = "black"),
+                   strip.text = element_text(size=25),
+                   panel.border = element_rect(colour = "black", fill=NA, size=1))
+
+# a really long way to create minor tick axis tick marks
+every_nth <- function(x, nth, empty = TRUE, inverse = FALSE) 
+{
+  if (!inverse) {
+    if(empty) {
+      x[1:nth == 1] <- ""
+      x
+    } else {
+      x[1:nth != 1]
+    }
+  } else {
+    if(empty) {
+      x[1:nth != 1] <- ""
+      x
+    } else {
+      x[1:nth == 1]
+    }
+  }
+}
+
+##pre-processing for Ta and Td data
+
+all <- read.csv("FINAL_BMdatabase_20210808.csv") %>%
+  mutate(Datetime = mdy_hm(Datetime))
+
+#get rid of the 01 minutes and make it 00
+minute(all$Datetime) <- 0
+
+all <- all[-c(1,2), ]
+
+############################################################################
+#data are set up - run models now for Ta (get slope, r2, and p-value)
+
+#slope, r2, deg of free, and pval for 2020 hourly
+fit_model <- function(all) lm(temperature ~ Elevation, data = all) #linear model
+get_slope <- function(model) tidy(model)$estimate[2] #pull out the slope
+pearson <- function(all) cor.test(all$Elevation, all$temperature, data=all) #pearson cor test
+pval <- function(pear) tidy(pear)$p.value #p value from cor test
+df <- function(pear) tidy(pear)$parameter[1] #deg of freedom from cor test
+
+#calculate r2
+allta_r <- all %>%
+  select(-c(ID)) %>%
+  group_by(Datetime) %>%
+  summarize(r2 = cor(Elevation, temperature, use="complete.obs")^2)
+
+#run lm, pull out slope, pearson cor test, pull out pval + deg of free
+slope_ta <- all %>%
+  group_nest(Datetime) %>%
+  mutate(model = map(data, fit_model)) %>%
+  mutate(slope = map_dbl(model, get_slope)) %>%
+  mutate(slope_Ckm = slope*1000) %>%
+  mutate(pear = map(data, pearson)) %>%
+  mutate(pval = map_dbl(pear, pval)) %>%
+  mutate(df = map_dbl(pear, df))
+
+#add derived r2 to rest of data
+slope_ta <- slope_ta %>%
+  add_column(r2 = allta_r$r2)
+
+###########################################################################
+
+PLOT = "Ta gradient by r2_allseasons"
+ggplot(slope_ta, aes(x=Datetime, y=slope_Ckm)) +
+  geom_point(aes(colour=r2), size=2) + 
+  labs(x= "Date", y=expression("Air Temperature Gradient " (degree*C/km)), color=expression(paste("R"^2))) +
+  scale_x_datetime(date_labels = "%b-%y", date_break = "2 months") +
+  PlotFormat +
+  geom_hline(aes(yintercept=-6.5, linetype="ELR"), color="Red", size=1) +
+  scale_color_gradient(low='grey', high='black')+
+  scale_linetype_manual(name ="", values = c('solid')) +
+  scale_y_continuous(breaks=seq(-100, 100, 10)) 
+  
+ggsave(paste(PLOT,".png",sep=""), width = 15, height = 9)
+
+
+PLOT = "Ta gradient by pval_allseasons"
+ggplot(slope_ta, aes(x=Datetime, y=slope_Ckm)) +
+  geom_point(aes(colour=cut(pval, c(-Inf, 0.05, Inf))), size=2) + 
+  scale_x_datetime(date_labels = "%b-%y", date_break = "2 months") +
+  PlotFormat +
+  scale_color_manual(name = "pval",
+                     values = c("black","gray"),
+                     labels = c("S", "NS")) +
+  labs(x="", y=expression("Air Temperature Gradient " (degree*C/km))) +
+  guides(colour=guide_legend(title="p-value")) +
+  scale_y_continuous(breaks=seq(-100, 100, 10)) +
+  geom_hline(aes(yintercept=-6.5, linetype="ELR"), color="Red", size=1) +
+  scale_linetype_manual(name ="", values = c('solid'))
+
+ggsave(paste(PLOT,".png",sep=""), width = 15, height = 9)
+
+###############################################################################
+#now do for Td
+
+#datd are set up - run models now for td (get slope, r2, and p-value)
+
+#slope, r2, deg of free, and pval for 2020 hourly
+fit_model <- function(all) lm(dewpoint ~ Elevation, data = all) #linear model
+get_slope <- function(model) tidy(model)$estimate[2] #pull out the slope
+pearson <- function(all) cor.test(all$Elevation, all$dewpoint, data=all) #pearson cor test
+pval <- function(pear) tidy(pear)$p.value #p value from cor test
+df <- function(pear) tidy(pear)$parameter[1] #deg of freedom from cor test
+
+#calculate r2
+alltd_r <- all %>%
+  select(-c(ID)) %>%
+  group_by(Datetime) %>%
+  summarize(r2 = cor(Elevation, dewpoint, use="complete.obs")^2)
+
+#run lm, pull out slope, pearson cor test, pull out pval + deg of free
+slope_td <- all %>%
+  group_nest(Datetime) %>%
+  mutate(model = map(data, fit_model)) %>%
+  mutate(slope = map_dbl(model, get_slope)) %>%
+  mutate(slope_Ckm = slope*1000) %>%
+  mutate(pear = map(data, pearson)) %>%
+  mutate(pval = map_dbl(pear, pval)) %>%
+  mutate(df = map_dbl(pear, df))
+
+#add derived r2 to rest of data
+slope_td <- slope_td %>%
+  add_column(r2 = alltd_r$r2)
+#############################################################################
+#Td plots
+
+PLOT = "Td gradient by r2_allseasons"
+ggplot(slope_td, aes(x=Datetime, y=slope_Ckm)) +
+  geom_point(aes(colour=r2), size=2) + 
+  labs(x= "Date", y=expression("Dewpoint Temperature Gradient " (degree*C/km)), color=expression(paste("R"^2))) +
+  scale_x_datetime(date_labels = "%b-%y", date_break = "2 months") +
+  PlotFormat +
+  geom_hline(aes(yintercept=-5.1, linetype="L&E 2006"), color="Red", size=1) +
+  scale_color_gradient(low='grey', high='black')+
+  scale_linetype_manual(name ="", values = c('solid')) +
+  scale_y_continuous(breaks=seq(-100, 100, 10)) 
+
+ggsave(paste(PLOT,".png",sep=""), width = 15, height = 9)
+
+
+PLOT = "Td gradient by pval_allseasons"
+ggplot(slope_td, aes(x=Datetime, y=slope_Ckm)) +
+  geom_point(aes(colour=cut(pval, c(-Inf, 0.05, Inf))), size=2) + 
+  scale_x_datetime(date_labels = "%b-%y", date_break = "2 months") +
+  PlotFormat +
+  scale_color_manual(name = "pval",
+                     values = c("black","gray"),
+                     labels = c("S", "NS")) +
+  labs(x="", y=expression("Dewpoint Temperature Gradient " (degree*C/km))) +
+  guides(colour=guide_legend(title="p-value")) +
+  scale_y_continuous(breaks=seq(-100, 100, 10)) +
+  geom_hline(aes(yintercept=-5.1, linetype="L&E 2006"), color="Red", size=1) +
+  scale_linetype_manual(name ="", values = c('solid'))
+
+ggsave(paste(PLOT,".png",sep=""), width = 15, height = 9)
+
+##############################################################################
+#now split by snow season (peakl to SAG, SAG to start, start to peak, peak to SAG)
